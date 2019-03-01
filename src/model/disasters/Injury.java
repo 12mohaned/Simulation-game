c2118bea1805df9c4fcd997a93cc640c267992fd
mohaned-mashaly <mohaned_boss@outlook.com>
2019-03-01 20:53:05 +0200

    Disaster happens to Citizen

--- src/model/disasters/Injury.java
+++ src/model/disasters/Injury.java
@@ -1,8 +1,7 @@
 package model.disasters;
-
 import model.people.Citizen;
 import simulation.Rescuable;
-
+//Disaster happens to Citizen
 public class Injury extends Disaster{
 public	Injury(int cycle, Citizen target){
 	super(cycle,target);	
