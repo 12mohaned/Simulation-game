06ed33faba59ae8cd90eed5b9a89afa49cafeead
mohaned-mashaly <mohaned_boss@outlook.com>
2019-03-01 20:51:36 +0200

    A disaster happens to Building

--- src/model/disasters/GasLeak.java
+++ src/model/disasters/GasLeak.java
@@ -1,8 +1,7 @@
 package model.disasters;
-
+//Disaster which happens to Building
 import model.infrastructure.ResidentialBuilding;
 import simulation.Rescuable;
-
 public class GasLeak extends Disaster{
 public	GasLeak(int cycle, ResidentialBuilding target) {
 	super(cycle,target);
