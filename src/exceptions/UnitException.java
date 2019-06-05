d483b15996aae08c0785bb05a1a1bd25819b38ce
mohaned-mashaly <mohaned_boss@outlook.com>
2019-06-05 03:38:57 +0200

    Exception Handles Buildings collapsed to not by hit by Disasters Again

---
+++ src/exceptions/UnitException.java
@@ -0,0 +1,17 @@
+package exceptions;
+import model.units.Unit;
+import simulation.Rescuable;
+abstract public class UnitException extends SimulationException{
+Unit unit;	
+Rescuable target;
+public UnitException(Unit unit, Rescuable target) {
+super();	
+this.unit = unit;
+this.target = target;
+}
+public UnitException(Unit unit, Rescuable target,String message){
+super(message);	
+this.unit = unit;
+this.target = target;
+}
+}
