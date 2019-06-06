99eb5d2e7a79640c6a7673049b0eba0500db0365
mohaned-mashaly <mohaned_boss@outlook.com>
2019-06-06 19:57:34 +0200

    Class represents Citizen in the game

--- src/model/people/Citizen.java
+++ src/model/people/Citizen.java
@@ -1,63 +1,138 @@
 package model.people;
-import simulation.*;
+
+import simulation.Address;
+import simulation.Rescuable;
+import simulation.Simulatable;
 import model.disasters.Disaster;
-public class Citizen implements Simulatable,Rescuable {
-private CitizenState state=CitizenState.SAFE;;
-private Disaster disaster;
-private Address location;
-private String nationalID;
-private String name;
-private int age;
-private int hp = 100;
-private int bloodLoss =0;
-private int toxicity = 0;
-//Citizen Class
-public Citizen(Address location, String nationalID, String name, int age){
-this.location = location;
-this.nationalID = nationalID;
-this.name= name;
-this.age = age;
-}
-public CitizenState getState(){
-return state;	
-}
-public void setState(CitizenState state){
-this.state = state;	
-}
-public Disaster getDisaster(){
-return disaster;	
-}
-public Address getLocation(){
-return location;	
-}
-public void setLocation(Address Location){
-location = Location;	
-}
-public String getNationalID(){
-return nationalID;	
-}
-public String getName(){
-return name;	
-}
-public int getAge(){
-return age;	
-}
-public int getHp(){
-return hp;	
-}
-public void setHp(int hp){
-this.hp = hp;	
-}
-public int getBloodLoss(){
-return bloodLoss;	
-}
-public void setBloodLoss(int bloodloss){
-bloodLoss = bloodloss; 	
-}
-public int getToxicity(){
-return toxicity;	
-}
-public void setToxicity(int toxicity){
-this.toxicity = toxicity;	
-}
+import model.events.SOSListener;
+import model.events.WorldListener;
+
+public class Citizen implements Rescuable,Simulatable{
+	private CitizenState state;
+	private Disaster disaster;
+	private String name;
+	private String nationalID;
+	private int age;
+	private int hp;
+	private int bloodLoss;
+	private int toxicity;
+	private Address location;
+	private SOSListener emergencyService;
+	private WorldListener worldListener;
+	public Citizen(Address location,String nationalID, String name, int age
+			,WorldListener worldListener) {
+		this.name = name;
+		this.nationalID = nationalID;
+		this.age = age;
+		this.location = location;
+		this.state=CitizenState.SAFE;
+		this.hp=100;
+		this.worldListener = worldListener;
+	}
+	
+	public WorldListener getWorldListener() {
+		return worldListener;
+	}
+
+	public void setWorldListener(WorldListener listener) {
+		this.worldListener = listener;
+	}
+
+	public CitizenState getState() {
+		return state;
+	}
+	public void setState(CitizenState state) {
+		this.state = state;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+		if(this.hp>=100)
+			this.hp=100;
+		else if(this.hp<=0){
+			this.hp = 0;
+			state=CitizenState.DECEASED;
+		}
+	}
+	public int getBloodLoss() {
+		return bloodLoss;
+	}
+	public void setBloodLoss(int bloodLoss) {
+		this.bloodLoss = bloodLoss;
+		if(bloodLoss<=0)
+			this.bloodLoss=0;
+		else if(bloodLoss>=100)
+		{
+			this.bloodLoss=100;
+			setHp(0);
+		}
+	}
+	public int getToxicity() {
+		return toxicity;
+	}
+	public void setToxicity(int toxicity) {
+		this.toxicity = toxicity;
+		if(toxicity>=100)
+		{
+			this.toxicity=100;
+			setHp(0);
+		}
+		else if(this.toxicity<=0)
+			this.toxicity=0;
+	}
+	public Address getLocation() {
+		return location;
+	}
+	public void setLocation(Address location) {
+		this.location = location;
+	}
+	public Disaster getDisaster() {
+		return disaster;
+	}
+	public String getNationalID() {
+		return nationalID;
+	}
+	
+	public void setEmergencyService(SOSListener emergency) {
+		this.emergencyService = emergency;
+	}
+	@Override
+	public void cycleStep() {
+		if(bloodLoss>0 && bloodLoss<30)
+			setHp(hp-5);
+		else if(bloodLoss>=30 && bloodLoss<70)
+			setHp(hp-10);
+		else if(bloodLoss >=70)
+			setHp(hp-15);
+		if (toxicity >0 && toxicity < 30)
+			setHp(hp-5);
+		else if(toxicity>=30 &&toxicity<70)
+			setHp(hp-10);
+		else if(toxicity>=70)
+			setHp(hp-15);
+	}
+	@Override
+	public void struckBy(Disaster d) {
+		if(disaster!=null)
+			disaster.setActive(false);
+		disaster=d;
+		state= CitizenState.IN_TROUBLE;
+		emergencyService.receiveSOSCall(this);
+		
+	}
+	public String toString() {
+	String s = "";
+	s = "Citizen Name : " +this.name + "\n" + "Citizen State : " + state + "\n" + "Citizen National Id :" + nationalID + "\n" + "Citizen Age :" + age + "\n"+
+	"Citizen Hp:" + hp + "\n" + "Citizen BloodLoss :" + bloodLoss + "\n" + "Toxicity Level :" + toxicity + "Citizen Location:" + "(" +this.getLocation().getX()+
+	"," + this.getLocation().getY() + ")" ;
+	return s;
+	}
 }
